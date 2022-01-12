e189b0e102af6965a18c95970556b4b697e845f0
Konstantin Didenko <ironbullyk@gmail.com>
2022-01-12 21:00:25 +0200

    first commit

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Введите ваше имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Введите ваш эмайл")]
+        //[RegularExpression(".+\\@.+\\ .. +", ErrorMessage = "Please enter а valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Введите ваш номер телефона")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Выберите ответ из предложенных вариантов")]
+        public bool? WillAtend { get; set; }
+    }
+}
---
+++ PartyInvites/Models/Party.cs
@@ -0,0 +1,7 @@
+﻿namespace PartyInvites.Models
+{
+    public  class Party
+    {
+        public readonly DateTime DateOfParty = new DateTime(2022, 10, 10, 17, 0, 0);
+    }
+}
